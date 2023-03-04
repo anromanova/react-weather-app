a71873a17230c6ee5c48b25a94158f8441d362e0
Anastasia Romanova <anastasiya.erohina@gmail.com>
2023-03-04 22:13:12 +0000

    add basic styles

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "ddfa5466c8msh13f9377dce69403p1f4002jsndccdec3bda08",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
